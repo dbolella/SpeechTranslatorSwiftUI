16afd79f3036f0fa75d3677a6ba7d6cb64814e8e
Danny Bolella <dbolella@syncro-tech.com>
2019-10-09 10:41:58 -0400

    Initial Commit

---
+++ SpeechTranslator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SpeechTranslator
+//
+//  Created by Daniel Bolella on 10/9/19.
+//  Copyright © 2019 Daniel Bolella. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
